063979d7da5c584f3fdbf8292d75d051df21c2ef
Hamze Abou Daher <hamzeaboudaher@gmail.com>
2024-03-06 16:09:37 +0100

    music hinfügen

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function startGame() {
 
 
 
-// (attempts === 1) ? randomNumber > 500 : randomNumber < 500 ;
 
 function guessNumber() {
   let guessInput = document.getElementById("guessInput");
@@ -24,20 +23,47 @@ function guessNumber() {
 
   if (guess < secretNumber) {
     document.getElementById("message").innerText = "Too low! Try again.";
+    playFailSound(); // Play fail sound
   } else if (guess > secretNumber) {
     document.getElementById("message").innerText = "Too high! Try again.";
+    playFailSound(); // Play fail sound
   } else {
     document.getElementById(
       "message"
     ).innerText = `Congratulations! You guessed the number in ${attempts} attempts.`;
-    // startGame(); // Start a new game after winning
+    playSuccessSound(); // Play success sound
+    setTimeout(function () {
+      document.getElementById("message").innerText = "play again!!";
+    }, 10000); // 10000 milliseconds = 10 seconds
+    startGame(); // Start a new game after winning
     setTimeout(function () {
       guessInput.value = ""; // Clear the input field after 5 seconds
-    }, 5000);
-    setTimeout(function () {
-      document.getElementById("message").innerText = " play again!!";
-    }, 5000);
+    }, 5000); // 5000 milliseconds = 5 seconds
   }
 }
+
+function playFailSound() {
+  let failSound = new Audio("fail.mp3");
+  failSound.play();
+}
+
+function playSuccessSound() {
+  let successSound = new Audio("win.mp3");
+  successSound.play();
+}
+
+
+let backgroundSound = document.getElementById("backgroundSound");
+
+function toggleBackgroundSound() {
+  if (backgroundSound.paused) {
+    backgroundSound.play();
+    document.getElementById("soundButton").innerText = "Turn Sound Off";
+  } else {
+    backgroundSound.pause();
+    document.getElementById("soundButton").innerText = "Turn Sound On";
+  }
+}
+
 
 startGame();
