5ecd7fd5b97c155695f3f15bf30b76d86fd45de2
Hamze Abou Daher <hamzeaboudaher@gmail.com>
2024-03-06 16:28:26 +0100

    kk

--- app.js
+++ app.js
@@ -1,69 +1,139 @@
-let secretNumber;
-let attempts = 0;
+// let secretNumber;
+// let attempts = 0;
 
-function startGame() {
-  secretNumber = Math.floor(Math.random() * 1000) + 1;
-  attempts = 0;
-}
-
-
+// function startGame() {
+//   secretNumber = Math.floor(Math.random() * 1000) + 1;
+//   attempts = 0;
+// }
 
 
-function guessNumber() {
-  let guessInput = document.getElementById("guessInput");
-  let guess = parseInt(document.getElementById("guessInput").value);
 
-  if (isNaN(guess) || guess < 1 || guess > 1000) {
-    document.getElementById("message").innerText =
-      "Please enter a valid number between 1 and 1000.";
-    return;
+
+// function guessNumber() {
+//   let guessInput = document.getElementById("guessInput");
+//   let guess = parseInt(document.getElementById("guessInput").value);
+
+//   if (isNaN(guess) || guess < 1 || guess > 1000) {
+//     document.getElementById("message").innerText =
+//       "Please enter a valid number between 1 and 1000.";
+//     return;
+//   }
+
+//   attempts++;
+
+//   if (guess < secretNumber) {
+//     document.getElementById("message").innerText = "Too low! Try again.";
+//     playFailSound(); // Play fail sound
+//   } else if (guess > secretNumber) {
+//     document.getElementById("message").innerText = "Too high! Try again.";
+//     playFailSound(); // Play fail sound
+//   } else {
+//     document.getElementById(
+//       "message"
+//     ).innerText = `Congratulations! You guessed the number in ${attempts} attempts.`;
+//     playSuccessSound(); // Play success sound
+//     setTimeout(function () {
+//       document.getElementById("message").innerText = "play again!!";
+//     }, 10000); // 10000 milliseconds = 10 seconds
+//     startGame(); // Start a new game after winning
+//     setTimeout(function () {
+//       guessInput.value = ""; // Clear the input field after 5 seconds
+//     }, 5000); // 5000 milliseconds = 5 seconds
+//   }
+// }
+
+// function playFailSound() {
+//   let failSound = new Audio("fail.mp3");
+//   failSound.play();
+// }
+
+// function playSuccessSound() {
+//   let successSound = new Audio("win.mp3");
+//   successSound.play();
+// }
+
+
+// let backgroundSound = document.getElementById("backgroundSound");
+
+// function toggleBackgroundSound() {
+//   if (backgroundSound.paused) {
+//     backgroundSound.play();
+//     document.getElementById("soundButton").innerText = "Turn Sound Off";
+//   } else {
+//     backgroundSound.pause();
+//     document.getElementById("soundButton").innerText = "Turn Sound On";
+//   }
+// }
+
+
+// startGame();
+
+if (typeof window !== "undefined") {
+  // Code, der nur im Browser ausgeführt wird
+  let secretNumber;
+  let attempts = 0;
+
+  function startGame() {
+    secretNumber = Math.floor(Math.random() * 1000) + 1;
+    attempts = 0;
   }
 
-  attempts++;
+  function guessNumber() {
+    let guessInput = document.getElementById("guessInput");
+    let guess = parseInt(guessInput.value);
 
-  if (guess < secretNumber) {
-    document.getElementById("message").innerText = "Too low! Try again.";
-    playFailSound(); // Play fail sound
-  } else if (guess > secretNumber) {
-    document.getElementById("message").innerText = "Too high! Try again.";
-    playFailSound(); // Play fail sound
-  } else {
-    document.getElementById(
-      "message"
-    ).innerText = `Congratulations! You guessed the number in ${attempts} attempts.`;
-    playSuccessSound(); // Play success sound
-    setTimeout(function () {
-      document.getElementById("message").innerText = "play again!!";
-    }, 10000); // 10000 milliseconds = 10 seconds
-    startGame(); // Start a new game after winning
-    setTimeout(function () {
-      guessInput.value = ""; // Clear the input field after 5 seconds
-    }, 5000); // 5000 milliseconds = 5 seconds
+    if (isNaN(guess) || guess < 1 || guess > 1000) {
+      document.getElementById("message").innerText =
+        "Please enter a valid number between 1 and 1000.";
+      return;
+    }
+
+    attempts++;
+
+    if (guess < secretNumber) {
+      document.getElementById("message").innerText = "Too low! Try again.";
+      playFailSound(); // Play fail sound
+    } else if (guess > secretNumber) {
+      document.getElementById("message").innerText = "Too high! Try again.";
+      playFailSound(); // Play fail sound
+    } else {
+      document.getElementById(
+        "message"
+      ).innerText = `Congratulations! You guessed the number in ${attempts} attempts.`;
+      playSuccessSound(); // Play success sound
+      setTimeout(function () {
+        document.getElementById("message").innerText = "play again!!";
+      }, 10000); // 10000 milliseconds = 10 seconds
+      startGame(); // Start a new game after winning
+      setTimeout(function () {
+        guessInput.value = ""; // Clear the input field after 5 seconds
+      }, 5000); // 5000 milliseconds = 5 seconds
+    }
   }
-}
 
-function playFailSound() {
-  let failSound = new Audio("fail.mp3");
-  failSound.play();
-}
-
-function playSuccessSound() {
-  let successSound = new Audio("win.mp3");
-  successSound.play();
-}
-
-
-let backgroundSound = document.getElementById("backgroundSound");
-
-function toggleBackgroundSound() {
-  if (backgroundSound.paused) {
-    backgroundSound.play();
-    document.getElementById("soundButton").innerText = "Turn Sound Off";
-  } else {
-    backgroundSound.pause();
-    document.getElementById("soundButton").innerText = "Turn Sound On";
+  function playFailSound() {
+    let failSound = new Audio("fail.mp3");
+    failSound.play();
   }
+
+  function playSuccessSound() {
+    let successSound = new Audio("win.mp3");
+    successSound.play();
+  }
+
+  let backgroundSound = document.getElementById("backgroundSound");
+
+  function toggleBackgroundSound() {
+    if (backgroundSound.paused) {
+      backgroundSound.play();
+      document.getElementById("soundButton").innerText = "Turn Sound Off";
+    } else {
+      backgroundSound.pause();
+      document.getElementById("soundButton").innerText = "Turn Sound On";
+    }
+  }
+
+  startGame();
+} else {
+  console.log("Dieser Code wird nur im Browser ausgeführt.");
 }
-
-
-startGame();
